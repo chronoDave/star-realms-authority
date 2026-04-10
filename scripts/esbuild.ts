98a48ebd2f7cb963cbf144274c3a8f3e5126bd35
chronoDave <davidwolters6758@gmail.com>
2026-04-10 20:30:05 +0100

    Improved build

---
+++ scripts/esbuild.ts
@@ -0,0 +1,61 @@
+import type { BuildOptions } from 'esbuild';
+
+import esbuild from 'esbuild';
+import fsp from 'fs/promises';
+import path from 'path';
+
+const isWatch = process.argv.slice(2)[0] === '-w';
+const outdir = 'docs';
+const version = await fsp.readFile('package.json', 'utf-8')
+  .then(raw => JSON.parse(raw).version);
+
+const config: BuildOptions = {
+  entryPoints: [
+    'src/index.html'
+  ],
+  bundle: true,
+  minify: !isWatch,
+  sourcemap: isWatch,
+  target: 'es6',
+  loader: {
+    '.html': 'text'
+  },
+  outdir,
+  plugins: [{
+    name: 'log',
+    setup: build => {
+      const label = 'esbuild';
+
+      build.onStart(() => {
+        console.time(label);
+      });
+
+      build.onEnd(() => {
+        console.timeEnd(label);
+      });
+    }
+  }, {
+    name: 'html',
+    setup: build => {
+      build.onLoad({ filter: /\.html$/ }, async args => {
+        const raw = await fsp.readFile(args.path, 'utf-8');
+
+        return {
+          contents: raw.replace(/\${version}/gi, version),
+          loader: 'copy'
+        };
+      });
+    }
+  }]
+};
+
+await fsp.rm(outdir, { recursive: true, force: true });
+await fsp.mkdir(outdir);
+await fsp.cp('src/assets', path.join(outdir, 'assets'), { recursive: true });
+
+if (isWatch) {
+  const context = await esbuild.context(config);
+  context.watch();
+} else {
+  esbuild.build(config);
+}
